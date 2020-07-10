f6ab1a4b2942836461358e01ca0edb8c1860761e
MiniCongee <replybber@gmail.com>
2020-07-10 20:21:15 +0800

    completed Money View Types Component CSS & Add helper.scss reset.scss

---
+++ src/components/Money/Types.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="typesWrapper">
+    <ul class="types">
+      <li class="left selected">流出</li>
+      <li class="right">流入</li>
+      <li class="cancel">
+        <Icon name="cancel"/>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Types'
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
+  .typesWrapper {
+    background: #FFE7E4;
+
+    .types {
+      display: flex;
+      justify-content: center;
+      text-align: center;
+      font-size: 12px;
+      position: relative;
+
+      > li {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 26px;
+        margin-top: 12px;
+        margin-bottom: 12px;
+        color: #A45240;
+        border: 1px solid #A45240;
+      }
+
+      > li.left {
+        width: 38%;
+        border-radius: 4px 0 0 4px;
+      }
+
+      > li.right {
+        width: 38%;
+        border-radius: 0 4px 4px 0;
+      }
+
+      > li.selected {
+        background: #A45240;
+        color: #FFFFFF;
+      }
+
+      > li.cancel {
+        position: absolute;
+        right: 10px;
+        border: none;
+
+        > .icon {
+          width: 20px;
+          height: 20px;
+        }
+      }
+    }
+  }
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,11 +1,61 @@
 <template>
   <Layout>
-    Money.vue
+    <Types/>
+
+    <div class="amountWrapper">
+      <ul class="amount">
+        <li>账单金额</li>
+        <li class="output">0.00</li>
+      </ul>
+    </div>
+    <div class="tagsWrapper">
+      <ul class="tags">
+        <li>餐饮</li>
+        <li>零食烟酒</li>
+        <li>购物</li>
+        <li>住房</li>
+        <li>交通</li>
+        <li>娱乐</li>
+        <li>文教</li>
+        <li>汽车</li>
+        <li>通讯</li>
+        <li>育儿</li>
+      </ul>
+    </div>
+    <div class="notesWrapper">
+      <label class="notes">
+        <span class="date">今天</span>
+        <input type="text" placeholder="请输入备注信息">
+      </label>
+    </div>
+    <div class="numberPad">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>删除</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>Add</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>确定</button>
+      <button>清空</button>
+      <button>0</button>
+      <button>.</button>
+    </div>
   </Layout>
 </template>
 
 <script lang="ts">
+  import Types from '@/components/Money/Types.vue';
   export default {
     name: 'Money',
+    components: {Types},
   };
 </script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+</style>
