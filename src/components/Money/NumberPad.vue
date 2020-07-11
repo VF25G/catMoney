147eb0588ec7fd4317593bbf7b74da22353f5756
MiniCongee <replybber@gmail.com>
2020-07-11 15:55:34 +0800

    updated hepler.scss & completed NumberPad Component CSS/HTML

---
+++ src/components/Money/NumberPad.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="numberPad">
+    <button>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>
+      <Icon name="delete"/>
+    </button>
+    <button>4</button>
+    <button>5</button>
+    <button>6</button>
+    <button>
+      <Icon name="add"/>
+    </button>
+    <button>7</button>
+    <button>8</button>
+    <button>9</button>
+    <button class="ok">确定</button>
+    <button>清空</button>
+    <button>0</button>
+    <button>.</button>
+  </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NumberPad'
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+
+  .numberPad {
+    @extend %clearFix;
+
+    > button {
+      width: 25%;
+      height: 56px;
+      float: left;
+      background: #FFE7E4;
+      border: none;
+      border-bottom: 2px solid #FED8D4;
+      border-right: 2px solid #FED8D4;
+      font-size: 32px;
+      color: #A45240;
+
+      &.ok {
+        height: 56*2px;
+        float: right;
+      }
+
+      &:nth-child(12) {
+        font-size: 26px;
+        border: none;
+        background: #57CDE1;
+        color: #FFFFFF;
+      }
+
+      &:nth-child(13) {
+        font-size: 20px;
+      }
+
+      &:nth-child(8) {
+        border: none;
+      }
+
+      &:nth-child(4), &:nth-child(8), &:nth-child(11), &:nth-child(15) {
+        border-right: none;
+      }
+
+      &:nth-child(13), &:nth-child(14), &:nth-child(15) {
+        border-bottom: none;
+      }
+    }
+  }
+</style>
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,11 @@
 <template>
-  <Layout>
+  <div>
     <Types/>
     <Amount/>
     <Tags/>
     <Notes/>
-
-    <div class="numberPad">
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>删除</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>Add</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>确定</button>
-      <button>清空</button>
-      <button>0</button>
-      <button>.</button>
-    </div>
-  </Layout>
+    <NumberPad/>
+  </div>
 </template>
 
 <script lang="ts">
@@ -30,10 +13,11 @@
   import Amount from '@/components/Money/Amount.vue';
   import Tags from '@/components/Money/Tags.vue';
   import Notes from '@/components/Money/Notes.vue';
+  import NumberPad from '@/components/Money/NumberPad.vue';
 
   export default {
     name: 'Money',
-    components: {Notes, Tags, Amount, Types},
+    components: {NumberPad, Notes, Tags, Amount, Types},
   };
 </script>
 
