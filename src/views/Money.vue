5ace3c7a5bbba4b7726e0faa55f6f6c91e229c6a
MiniCongee <replybber@gmail.com>
2020-07-16 10:04:09 +0800

    add receiptTagsList

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <Types/>
     <Amount/>
-    <Tags :data-source="tags" class="tags"/>
+    <Tags :data-source="disburseTags" class="tags"/>
     <Notes/>
     <NumberPad class="numberPad"/>
   </div>
@@ -21,7 +21,7 @@
     components: {NumberPad, Notes, Tags, Amount, Types}
   })
   export default class Money extends Vue {
-    tags = [
+    disburseTags = [
       {icon:'food', name:'餐饮'},
       {icon:'snacks', name:'烟酒零食'},
       {icon:'shop', name:'购物'},
@@ -33,6 +33,19 @@
       {icon:'communication', name:'通讯'},
       {icon:'parenting', name:'育儿'}
     ];
+    receiptTags = [
+      {icon:'salary', name:'薪资'},
+      {icon:'bonus', name:'奖金'},
+      {icon:'borrow', name:'借入'},
+      {icon:'collectDebt', name:'收债'},
+      {icon:'interest', name:'利息收入'},
+      {icon:'investment', name:'投资回报'},
+      {icon:'incomeFromInvestment', name:'投资收益'},
+      {icon:'reimbursement', name:'报销收入'},
+      {icon:'drawback', name:'退款'},
+      {icon:'windfall', name:'意外所得'}
+    ];
+
   }
 </script>
 
