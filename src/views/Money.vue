fa0f60808df75b7c3947d86248e5ed1f89642ef3
MiniCongee <replybber@gmail.com>
2020-07-16 14:55:28 +0800

    Money.vue .tags CSS style must having to apaptation another Phone

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -5,7 +5,8 @@
     <Tags :current-type="selectedType"
           :data-source="selectedType === '-'?
                         disburseTagsList:
-                        receiptTagsList"/>
+                        receiptTagsList"
+          class="tags"/>
     <Notes/>
     <NumberPad class="numberPad"/>
   </div>
@@ -27,28 +28,28 @@
   export default class Money extends Vue {
 
     disburseTagsList = [
-      {icon:'food', name:'餐饮'},
-      {icon:'snacks', name:'烟酒零食'},
-      {icon:'shop', name:'购物'},
-      {icon:'apartment', name:'住房'},
-      {icon:'trafficLight', name:'交通'},
-      {icon:'recreation', name:'娱乐'},
-      {icon:'education', name:'文教'},
-      {icon:'car', name:'汽车'},
-      {icon:'communication', name:'通讯'},
-      {icon:'parenting', name:'育儿'}
+      {icon: 'food', name: '餐饮'},
+      {icon: 'snacks', name: '烟酒零食'},
+      {icon: 'shop', name: '购物'},
+      {icon: 'apartment', name: '住房'},
+      {icon: 'trafficLight', name: '交通'},
+      {icon: 'recreation', name: '娱乐'},
+      {icon: 'education', name: '文教'},
+      {icon: 'car', name: '汽车'},
+      {icon: 'communication', name: '通讯'},
+      {icon: 'parenting', name: '育儿'}
     ];
     receiptTagsList = [
-      {icon:'salary', name:'薪资'},
-      {icon:'bonus', name:'奖金'},
-      {icon:'borrow', name:'借入'},
-      {icon:'collectDebt', name:'收债'},
-      {icon:'interest', name:'利息收入'},
-      {icon:'investment', name:'投资回报'},
-      {icon:'incomeFromInvestment', name:'投资收益'},
-      {icon:'reimbursement', name:'报销收入'},
-      {icon:'drawback', name:'退款'},
-      {icon:'windfall', name:'意外所得'}
+      {icon: 'salary', name: '薪资'},
+      {icon: 'bonus', name: '奖金'},
+      {icon: 'borrow', name: '借入'},
+      {icon: 'collectDebt', name: '收债'},
+      {icon: 'interest', name: '利息收入'},
+      {icon: 'investment', name: '投资回报'},
+      {icon: 'incomeFromInvestment', name: '投资收益'},
+      {icon: 'reimbursement', name: '报销收入'},
+      {icon: 'drawback', name: '退款'},
+      {icon: 'windfall', name: '意外所得'}
     ];
 
     selectedType = '-';
@@ -68,4 +69,10 @@
     display: flex;
     flex-direction: column;
   }
+
+  // tags 内容区域自动拉伸
+  .tags {
+    flex-grow: 3;
+  }
+
 </style>
