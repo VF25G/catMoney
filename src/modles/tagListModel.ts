3941e297af4fc6e808cdbaab72a6bf647f1611e0
MiniCongee <replybber@gmail.com>
2020-07-17 21:42:40 +0800

    updated tagListModel.ts && completed Create Tags in Labels.vue

--- src/modles/tagListModel.ts
+++ src/modles/tagListModel.ts
@@ -2,7 +2,7 @@ const localStorageKeyName = 'tagList';
 type TagListModel = {
   data: string[];
   fetch: () => string[];
-  create: (name: string) => string;
+  create: (name: string) => 'success' | 'duplicated';
   save: () => void;
 }
 const tagListModel: TagListModel = {
@@ -12,9 +12,10 @@ const tagListModel: TagListModel = {
     return this.data;
   },
   create(name: string) {
+    if (this.data.indexOf(name) >= 0) { return 'duplicated' }
     this.data.push(name);
     this.save();
-    return name;
+    return 'success';
   },
   save() {
     window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,14 +17,20 @@
   import {Component} from 'vue-property-decorator';
   import tagListModel from '@/modles/tagListModel';
 
+  tagListModel.fetch();
   @Component
   export default class Labels extends Vue {
-    tags = tagListModel.fetch();
+    tags = tagListModel.data;
 
     createTag() {
       const name = window.prompt('请输入标签名');
       if (name) {
-        tagListModel.create(name);
+        const message = tagListModel.create(name);
+        if (message === 'duplicated') {
+          window.alert('标签名重复');
+        } else if (message === 'success') {
+          window.alert('添加成功');
+        }
       }
     }
   }
