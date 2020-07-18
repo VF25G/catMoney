cc15e331e98e20b0881a04a7142e49f9068e664a
MiniCongee <replybber@gmail.com>
2020-07-18 15:35:09 +0800

    solved the ID create problem

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -44,12 +44,18 @@
         tagListModel.update(this.tag.id, name);
       }
     }
-    remove(){
-      if(this.tag){
-        tagListModel.remove(this.tag.id);
+
+    remove() {
+      if (this.tag) {
+        if (tagListModel.remove(this.tag.id)) {
+          this.$router.back();
+        } else {
+          window.alert('删除失败');
+        }
       }
     }
-    goBack(){
+
+    goBack() {
       this.$router.back();
     }
   }
