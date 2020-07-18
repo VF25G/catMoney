6010d17040bc52a76858ce56b2f2933d748f6bb3
MiniCongee <replybber@gmail.com>
2020-07-18 21:09:25 +0800

    package create/remove/update/find method for tagList

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -17,7 +17,6 @@
 <script lang="ts">
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
-  import tagListModel from '@/modles/tagListModel';
   import CustomButton from '@/components/CustomButton.vue';
 
   @Component({
