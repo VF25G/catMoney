a43d4f8618b89eaf189f0514283d49e23b35d25b
MiniCongee <replybber@gmail.com>
2020-07-20 00:07:51 +0800

    抽离TagHelper作为minxin

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -15,9 +15,10 @@
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import CustomButton from '@/components/CustomButton.vue';
+  import {mixins} from 'vue-class-component';
+  import TagHelper from '@/mixins/TagHelper';
 
   @Component({
     components: {CustomButton},
@@ -27,15 +28,9 @@
       }
     }
   })
-  export default class Labels extends Vue {
+  export default class Labels extends mixins(TagHelper) {
     beforeCreate() {
       this.$store.commit('fetchTags');
-    }
-
-    createTag() {
-      const name = window.prompt('请输入标签名');
-      if (!name) { return window.alert('标签名不能为空'); }
-      this.$store.commit('createTag', name);
     }
   }
 </script>
