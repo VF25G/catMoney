93897372bc067bd7f53a23c9d925d90a4ec190c4
MiniCongee <replybber@gmail.com>
2020-07-20 00:44:35 +0800

    use Vuex in EditLabel.vue

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -19,16 +19,18 @@
   import {Component} from 'vue-property-decorator';
   import FormItem from '@/components/Money/FormItem.vue';
   import CustomButton from '@/components/CustomButton.vue';
-  import store from '@/store/index2';
 
   @Component({
-    components: {CustomButton, FormItem}
+    components: {CustomButton, FormItem},
   })
   export default class EditLabel extends Vue {
-    tag?: Tag = undefined;
+    get tag() {
+      return this.$store.state.currentTag;
+    }
 
     created() {
-      this.tag = store.findTag(this.$route.params.id);
+      const id = this.$route.params.id;
+      this.$store.commit('setCurrentTag', id);
       if (!this.tag) {
         this.$router.replace('/404');
       }
@@ -36,17 +38,17 @@
 
     update(name: string) {
       if (this.tag) {
-        store.updateTag(this.tag.id, name);
+        // store.updateTag(this.tag.id, name);
       }
     }
 
     remove() {
       if (this.tag) {
-        if (store.removeTag(this.tag.id)) {
-          this.$router.back();
-        } else {
-          window.alert('删除失败');
-        }
+        // if (store.removeTag(this.tag.id)) {
+        //   this.$router.back();
+        // } else {
+        //   window.alert('删除失败');
+        // }
       }
     }
 
