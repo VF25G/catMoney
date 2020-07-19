a1d01005c3fea41200860e2bb00679cab68ba874
MiniCongee <replybber@gmail.com>
2020-07-19 23:51:12 +0800

    use Vuex in Labels.vue

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -18,19 +18,24 @@
   import Vue from 'vue';
   import {Component} from 'vue-property-decorator';
   import CustomButton from '@/components/CustomButton.vue';
-  import store from '@/store/index2';
 
   @Component({
-    components: {CustomButton}
+    components: {CustomButton},
+    computed: {
+      tags() {
+        return this.$store.state.tagList;
+      }
+    }
   })
   export default class Labels extends Vue {
-    tags = store.tagList;
+    beforeCreate() {
+      this.$store.commit('fetchTags');
+    }
 
     createTag() {
       const name = window.prompt('请输入标签名');
-      if (name) {
-        store.createTag(name);
-      }
+      if (!name) { return window.alert('标签名不能为空'); }
+      this.$store.commit('createTag', name);
     }
   }
 </script>
