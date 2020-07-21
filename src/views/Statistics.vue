e79497ae6cede856dcbf575645ad0f22e773969a
MiniCongee <replybber@gmail.com>
2020-07-21 15:46:18 +0800

    abstract Tabs.vue & used in Statistics.vue

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,11 +1,44 @@
 <template>
   <Layout>
-    Statistics.vue
+    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+    <Tabs class-prefix="interval" :data-source="IntervalList" :value.sync="interval"/>
   </Layout>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'Statistics'
-  };
+  import Vue from 'vue';
+  import Types from '@/components/Money/Types.vue';
+  import {Component} from 'vue-property-decorator';
+  import Tabs from '@/components/Tabs.vue';
+
+  @Component({
+    components: {Tabs, Types}
+  })
+  export default class Statistics extends Vue {
+    type = '-';
+    interval = 'day';
+    IntervalList = [
+      {text: '按天', value: 'day'},
+      {text: '按周', value: 'week'},
+      {text: '按月', value: 'month'}
+    ];
+    typeList = [
+      {text: '支出', value: '-'},
+      {text: '收入', value: '+'}
+    ];
+  }
 </script>
+
+<style scoped lang="scss">
+  ::v-deep .type-tabs-item {
+    background: white;
+
+    &.selected {
+      background: #C4C4C4;
+
+      &::after {
+        display: none;
+      }
+    }
+  }
+</style>
