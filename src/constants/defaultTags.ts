d13998b7150accec16eefb148cacfe5ff3941b2d
MiniCongee <replybber@gmail.com>
2020-08-02 16:05:09 +0800

    分离TagsList数据

---
+++ src/constants/defaultTags.ts
@@ -0,0 +1,27 @@
+const disburseTagsList = [
+  {icon: 'food', name: '餐饮'},
+  {icon: 'snacks', name: '烟酒零食'},
+  {icon: 'shop', name: '购物'},
+  {icon: 'apartment', name: '住房'},
+  {icon: 'trafficLight', name: '交通'},
+  {icon: 'recreation', name: '娱乐'},
+  {icon: 'education', name: '文教'},
+  {icon: 'car', name: '汽车'},
+  {icon: 'communication', name: '通讯'},
+  {icon: 'parenting', name: '育儿'}
+];
+
+const receiptTagsList = [
+  {icon: 'salary', name: '薪资'},
+  {icon: 'bonus', name: '奖金'},
+  {icon: 'borrow', name: '借入'},
+  {icon: 'collectDebt', name: '收债'},
+  {icon: 'interest', name: '利息收入'},
+  {icon: 'investment', name: '投资回报'},
+  {icon: 'incomeFromInvestment', name: '投资收益'},
+  {icon: 'reimbursement', name: '报销收入'},
+  {icon: 'drawback', name: '退款'},
+  {icon: 'windfall', name: '意外所得'}
+];
+
+export {disburseTagsList, receiptTagsList}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -24,6 +24,7 @@
   import NumberPad from '@/components/Money/NumberPad.vue';
   import {Component} from 'vue-property-decorator';
   import eventBus from '@/components/EventBus';
+  import {disburseTagsList, receiptTagsList} from '@/constants/defaultTags';
 
   @Component({
     components: {NumberPad, FormItem, Tags, Amount, Types}
@@ -39,30 +40,8 @@
       tags: '餐饮', notes: '', type: '-', amount: 0, createdAt: new Date().toISOString()
     };
 
-    disburseTagsList = [
-      {icon: 'food', name: '餐饮'},
-      {icon: 'snacks', name: '烟酒零食'},
-      {icon: 'shop', name: '购物'},
-      {icon: 'apartment', name: '住房'},
-      {icon: 'trafficLight', name: '交通'},
-      {icon: 'recreation', name: '娱乐'},
-      {icon: 'education', name: '文教'},
-      {icon: 'car', name: '汽车'},
-      {icon: 'communication', name: '通讯'},
-      {icon: 'parenting', name: '育儿'}
-    ];
-    receiptTagsList = [
-      {icon: 'salary', name: '薪资'},
-      {icon: 'bonus', name: '奖金'},
-      {icon: 'borrow', name: '借入'},
-      {icon: 'collectDebt', name: '收债'},
-      {icon: 'interest', name: '利息收入'},
-      {icon: 'investment', name: '投资回报'},
-      {icon: 'incomeFromInvestment', name: '投资收益'},
-      {icon: 'reimbursement', name: '报销收入'},
-      {icon: 'drawback', name: '退款'},
-      {icon: 'windfall', name: '意外所得'}
-    ];
+    disburseTagsList = disburseTagsList;
+    receiptTagsList = receiptTagsList;
 
     selectedType = '-';
 
@@ -108,8 +87,9 @@
   }
 
   .tags {
-    /*flex-grow: 3;*/   // tags 内容区域自动拉伸
-    padding-bottom: 80+56*5+12px;
+    /*flex-grow: 3;*/
+    // tags 内容区域自动拉伸
+    padding-bottom: 54*5px;
   }
 
   .numberPad {
