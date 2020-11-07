b3bb954f5e372aab336744fb7c07105b361c88b0
MiniCongee <replybber@gmail.com>
2020-11-08 00:24:01 +0800

    deploy

---
+++ precache-manifest.ee09e46d961bead1131f4f9d39fc55fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f07ecad8527c77c120d",
+    "url": "/catMoney/css/app.dc8f3519.css"
+  },
+  {
+    "revision": "61ceee0ba5ad87d0a01a2a2b20d55c76",
+    "url": "/catMoney/index.html"
+  },
+  {
+    "revision": "0f07ecad8527c77c120d",
+    "url": "/catMoney/js/app.61037ed6.js"
+  },
+  {
+    "revision": "c25c45ab35ddef738fcd",
+    "url": "/catMoney/js/chunk-vendors.a3e149e9.js"
+  },
+  {
+    "revision": "4d83f77e9b84c4a288d1069147073dd5",
+    "url": "/catMoney/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/catMoney/robots.txt"
+  }
+]);
